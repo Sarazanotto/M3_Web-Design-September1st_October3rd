d1b7846bc1d91076979c7630cd7b4f28e190a22c
sara zanotto <saraazanotto@gmail.com>
2025-09-25 18:00:56 +0200

    continuo esercizio W2D4

---
+++ W2/W2D4-consegna/main.js
@@ -0,0 +1,32 @@
+/*const welcomeSummerSection=document.querySelector(".welcome-summer")
+
+
+function generateCard(){
+const card=document.createElement("div")
+card.classList.add("general-card")
+card.style.width="300px"
+card.style.borderRadius="10px"
+
+
+const imgCard=document.createElement("img")
+imgCard.classList.add("img-card")
+imgCard.src="./assets/img3.jpg"
+imgCard.style.borderRadius="10px"
+imgCard.style.objectFit="cover"
+imgCard.width="100%"
+
+const textContainer=document.createElement("div")
+textContainer.classList.add("text-container")
+
+const titleCard=document.createElement("h4")
+titleCard.classList.add("title")
+
+const descriptionCard=document.createElement("p")
+descriptionCard.classList.add("description-card")
+
+textContainer.append(titleCard,descriptionCard)
+card.append(imgCard, textContainer)
+welcomeSummerSection.appendChild(card)
+
+}
+generateCard()*/
